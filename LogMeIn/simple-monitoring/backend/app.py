4e449f298516971363470448c279d22ff2dd190f
Kyban73 <157889266+Kyban73@users.noreply.github.com>
2025-12-07 12:24:16 +0100

    Update app.py

--- LogMeIn/simple-monitoring/backend/app.py
+++ LogMeIn/simple-monitoring/backend/app.py
@@ -260,4 +260,4 @@ def clear_logs():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
